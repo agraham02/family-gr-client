bd322e0f3b92965edc95ec270a0fc1f4a031ff8b
Ahmad Graham <ahmadgrahamdev@gmail.com>
2026-01-08 15:34:53 -0400

    refactor: remove unused useMemo import in TrickPile component

--- src/components/games/shared/TrickPile.tsx
+++ src/components/games/shared/TrickPile.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useMemo } from "react";
+import React from "react";
 import { cn } from "@/lib/utils";
 import { motion, AnimatePresence } from "motion/react";
 import { PlayingCard as PlayingCardType } from "@/types";
